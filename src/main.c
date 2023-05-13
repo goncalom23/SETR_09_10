0a7e6a2c1bab1c710b350ffa60debca6b3741387
filipempsilva <filipe.msilva@ua.pt>
2023-05-13 21:03:10 +0100

    Menu added

--- src/main.c
+++ src/main.c
@@ -14,26 +14,84 @@
 #define MOVIE_SELECTED 3
 
 extern uint8_t button_state[8];
+int credit;
+int select_movie;
+char* arrow_1 = "";
+char* arrow_2 = "";
+char* arrow_3 = "";
+char* arrow_4 = "";
+char* arrow_5 = "";
+
+
+void menu_movies(int select){
+        if(select == 1){
+			arrow_1 = "->";
+			arrow_2 = "";
+			arrow_3 = "";
+			arrow_4 = "";
+			arrow_5 = "";
+        }
+		else if(select == 2){
+			arrow_1 = "";
+			arrow_2 = "->";
+			arrow_3 = "";
+			arrow_4 = "";
+			arrow_5 = "";
+        }
+		else if(select == 3){
+			arrow_1 = "";
+			arrow_2 = "";
+			arrow_3 = "->";
+			arrow_4 = "";
+			arrow_5 = "";
+        }
+		else if(select == 4){
+			arrow_1 = "";
+			arrow_2 = "";
+			arrow_3 = "";
+			arrow_4 = "->";
+			arrow_5 = "";
+        }
+		else if(select == 5){
+			arrow_1 = "";
+			arrow_2 = "";
+			arrow_3 = "";
+			arrow_4 = "";
+			arrow_5 = "->";
+        }
+
+    printf("\033[2J\033[H");                                // clear window code
+    printf("\n CREDIT= %i",credit); 
+    printf("\n"); 
+    printf("\n%s Movie A, 19H00 session, 9 EUR",arrow_1); 
+    printf("\n%s Movie A, 21H00 session, 11 EUR",arrow_2); 
+    printf("\n%s Movie A, 23H00 session, 9 EUR",arrow_3);
+    printf("\n%s Movie B, 19H00 session, 10 EUR",arrow_4);
+    printf("\n%s Movie B, 21H00 session, 12 EUR",arrow_5);
+}
 
 void main(void)
 {   
         button_config();
 
-        int state = IDLE;        
-        while (1) 
+        credit = 0;
+		select_movie = 3;
+
+        int state = BROWSING_MOVIES;        
+        while (1)
         {
                 
-                k_msleep(100);
-                printf("\033[2J\033[H");                // clear window code
-                printk("button1: %i\n",button_state[0]);      
-                printk("button2: %i\n",button_state[1]);       
-                printk("button3: %i\n",button_state[2]);       
-                printk("button4: %i\n",button_state[3]);
-                printk("button5: %i\n",button_state[4]);      
-                printk("button6: %i\n",button_state[5]);       
-                printk("button7: %i\n",button_state[6]);       
-                printk("button8: %i\n",button_state[7]);       
-                 
+                k_msleep(400);
+                //printf("\033[2J\033[H");                // clear window code
+                //printk("button1: %i\n",button_state[0]);      
+                //printk("button2: %i\n",button_state[1]);       
+                //printk("button3: %i\n",button_state[2]);       
+                //printk("button4: %i\n",button_state[3]);
+                //printk("button5: %i\n",button_state[4]);      
+                //printk("button6: %i\n",button_state[5]);       
+                //printk("button7: %i\n",button_state[6]);       
+                //printk("button8: %i\n",button_state[7]);       
+                // 
 
                 switch(state)
                 {
@@ -45,7 +103,7 @@ void main(void)
 
                         case BROWSING_MOVIES:
                         {
-
+                                menu_movies(select_movie);
                                 break;
                         }
                         
