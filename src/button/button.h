ff33f1aff69f53b4961c9d1e0a7fd6c84eed0dc9
goncalom23 <goncalom23@ua.pt>
2023-05-15 11:36:42 +0100

    added new state, added doxygen comments, fixed tab spaces

--- src/button/button.h
+++ src/button/button.h
@@ -1,22 +1,33 @@
-#include <zephyr/kernel.h>          /* for k_msleep() */
-#include <zephyr/device.h>          /* for device_is_ready() and device structure */
-#include <zephyr/devicetree.h>		/* for DT_NODELABEL() */
-#include <zephyr/drivers/gpio.h>    /* for GPIO api*/
-#include <zephyr/sys/printk.h>      /* for printk()*/
+/*!
+ * \file:   button.h
+ * \author: Gonçalo Martins <goncalom23@ua.pt> and Filipe Silva <filipe.msilva@ua.pt>
+ * 
+ * \date May 15, 2023, 10:41 AM
+ * \brief
+ * Apropriate returns defines for the crc8 and respective function prototypes
+ */
+
+#include <zephyr/kernel.h>              /* for k_msleep() */
+#include <zephyr/device.h>              /* for device_is_ready() and device structure */
+#include <zephyr/devicetree.h>		    /* for DT_NODELABEL() */
+#include <zephyr/drivers/gpio.h>        /* for GPIO api*/
+#include <zephyr/sys/printk.h>          /* for printk()*/
 
 #ifndef button_H
 #define button_H
 
-/* Use a "big" sleep time to reduce CPU load (button detection int activated, not polled) */
-#define SLEEP_TIME_MS   60*1000 
+#define SLEEP_TIME_MS   60*1000         /* Use a "big" sleep time to reduce CPU load (button detection int activated, not polled) */
 
+/* Set the pins used for LED and buttons */
+/* LED 1 and buttons 1-4 are the ones on board */
+/* buttons 5-8 are connected to pins labeled A0 ... A3 (gpio0 pins 3,4,28,29) */
 #define LED1_PIN 13
 
-#define GPIO0_NODE DT_NODELABEL(gpio0)
+#define GPIO0_NODE DT_NODELABEL(gpio0)  /* Get node ID for GPIO0, which has leds and buttons */ 
 
-extern uint8_t button_state[8];
+extern uint8_t button_state[8];         /* array to track all buttons state */
+
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
-
 void button_config();
 
 
--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
+/*!
+ * \file:   main.c
+ * \author: Gonçalo Martins <goncalom23@ua.pt> and Filipe Silva <filipe.msilva@ua.pt>
+ * 
+ * \date May 15, 2023, 10:32 AM
+ * \brief
+ * SETR 22/23
+ * 
+ * 
+ */
+
 #include <zephyr.h>
 #include <device.h>
 #include <drivers/gpio.h>
@@ -11,6 +22,8 @@
 #define BROWSING_MOVIES 1
 #define ADD_CREDIT 2
 #define MOVIE_SELECTED 3
+#define RETURN_CREDIT 4
+
 
 char movie_name[5] = {'A','A','A','B','B'};
 uint8_t movie_price[5] = {9,11,9,10,12};
@@ -22,22 +35,41 @@ int select_movie = 0;
 uint32_t timer_counter = 0;
 char arrow [5][10];
 
-
+/** \brief
+ *  Processes the the chars received so far looking for commands 
+ *	\return                                                     
+ *	 0: if a valid command was found and executed           	
+ * 	-1: if empty string found            						 
+ * 	-2: if there is no valid command found                      
+ * 	-3: if a CS error is detected (command not executed)         
+ * 	-4: if string format is wrong 									
+ *	-5: incomplete command found                             	 
+ */ 
 void change_arrow()
 {
-        for(int i = 0; i<5; i++)
+    for(int i = 0; i<5; i++)
+    {
+        if(i == select_movie)
         {
-                if(i == select_movie)
-                {
-                        strcpy(arrow[i],"->");
-                }
-                else
-                {
-                        strcpy(arrow[i],"");
-                }
-        }     
+                strcpy(arrow[i],"->");
+        }
+        else
+        {
+                strcpy(arrow[i],"");
+        }
+    }     
 }
 
+/** \brief
+ *  Processes the the chars received so far looking for commands 
+ *	\return                                                     
+ *	 0: if a valid command was found and executed           	
+ * 	-1: if empty string found            						 
+ * 	-2: if there is no valid command found                      
+ * 	-3: if a CS error is detected (command not executed)         
+ * 	-4: if string format is wrong 									
+ *	-5: incomplete command found                             	 
+ */ 
 void print_interface()
 {
 
@@ -63,124 +95,138 @@ void print_button_state(){
 	printf("\nButton 8: %i", button_state[7]);
 }
 
-
+/** \brief
+ *  Processes the the chars received so far looking for commands 
+ *	\return                                                     
+ *	 0: if a valid command was found and executed           	
+ * 	-1: if empty string found            						 
+ * 	-2: if there is no valid command found                      
+ * 	-3: if a CS error is detected (command not executed)         
+ * 	-4: if string format is wrong 									
+ *	-5: incomplete command found                             	 
+ */ 
 void main(void)
 {   
-        button_config();
-		change_arrow();
+    button_config();
+    change_arrow();
 
-        int state = BROWSING_MOVIES;        
-        while (1)
+    int state = BROWSING_MOVIES;        
+    while (1)
+    {
+        k_msleep(10);
+        timer_counter++;
+        if(timer_counter == 50 && state == BROWSING_MOVIES)
         {
-                k_msleep(10);
-                timer_counter++;
-                if(timer_counter == 50 && state == BROWSING_MOVIES)
+            print_interface();
+			//print_button_state();
+            timer_counter = 0;
+        }
+        switch(state)
+        {
+            case BROWSING_MOVIES:
+            {
+                if(button_state[4] == 1)                // Browse Up
                 {
-                        print_interface();
-						//print_button_state();
-                        timer_counter = 0;
+                    select_movie = select_movie - 1; 
+                    change_arrow(); 
+                    button_state[4] = 0;                    
+                }
+                else if(button_state[5] == 1)           // Browse Down
+                {
+                    select_movie = select_movie + 1;
+                    change_arrow();     
+                    button_state[5] = 0;                    
+                }
+                else if(button_state[0] == 1 || button_state[1] == 1 || button_state[2] == 1 || button_state[3] == 1)   // add credit
+                {
+                    state = ADD_CREDIT;
+                } 
+                else if(button_state[6] == 1)           // Select
+                {
+                    state = MOVIE_SELECTED;
+                    button_state[6] = 0;                    
+                }
+                else if(button_state[7] == 1)           // Return credit
+                {
+                    state = RETURN_CREDIT;
+                }
+                
+                if(select_movie < 0)                    // Rotacao entre o ultimo e primeiro filme
+                {
+                    select_movie = 4;
+                    change_arrow();
+                }
+                if(select_movie > 4)
+                {
+                    select_movie = 0;
+                    change_arrow();
                 }
 
-                switch(state)
+                break;
+            }
+            
+            case ADD_CREDIT:
+            {
+                if(button_state[0] == 1)               // 1 EUR
                 {
-                        case BROWSING_MOVIES:
-                        {
-                                if(button_state[4] == 1)                // Browse Up
-                                {
-                                select_movie = select_movie - 1; 
-                                change_arrow(); 
-                                button_state[4] = 0;                    
-                                }
-                                else if(button_state[5] == 1)           // Browse Down
-                                {
-                                select_movie = select_movie + 1;
-                                change_arrow();     
-                                button_state[5] = 0;                    
-                                }
-
-                                if(select_movie < 0)                    // Rotacao entre o ultimo e primeiro filme
-                                {
-                                        select_movie = 4;
-                                        change_arrow();
-                                }
-                                if(select_movie > 4)
-                                {
-                                        select_movie = 0;
-                                        change_arrow();
-                                }
-
-                                if(button_state[0] == 1 || button_state[1] == 1 || button_state[2] == 1 || button_state[3] == 1 || button_state[7] == 1)
-                                {
-                                        state = ADD_CREDIT;
-                                } 
-
-
-                                if(button_state[6] == 1)                // Select
-                                {
-                                        state = MOVIE_SELECTED;
-                                        button_state[6] = 0;                    
-                                }
-                                break;
-                        }
-                        
-                        case ADD_CREDIT:
-                        {
-                                if(button_state[0] == 1)               // 1 EUR
-                                {
-                                credit = credit + 1;
-                                button_state[0] = 0;       
-                                }
-                                else if(button_state[1] == 1)           // 2 EUR
-                                {
-                                credit = credit + 2;
-                                button_state[1] = 0;                    
-                                }
-                                else if(button_state[2] == 1)           // 5 EUR
-                                {
-                                credit = credit + 5;
-                                button_state[2] = 0;                   
-                                }
-                                else if(button_state[3] == 1)           // 10 EUR
-                                {
-                                credit = credit + 10;            
-                                button_state[3] = 0;      
-                                }
-                                else if(button_state[7] == 1)           // Return
-                                {
-                                credit = 0;   
-                                printf("\033[2J\033[H");                                // clear window code
-                                printf("\n %i EUR return",credit);
-                                k_msleep(3000);
-                                button_state[7] = 0;      
-                                }
-
-                                state = BROWSING_MOVIES;
-                                break;
-                        }
-
-                        case MOVIE_SELECTED:
-                        {
-                                printf("\033[2J\033[H");                                // clear window code
-                                printf("\n Ticket for movie %c, session %uH00 issued",movie_name[select_movie],movie_session[select_movie]);
-
-                                if(movie_price[select_movie] > credit)
-                                {
-                                        printf("\n Not enought credit. Ticket not issued");                
-                                }
-                                else if(movie_price[select_movie] < credit)
-                                {
-                                        credit = credit-movie_price[select_movie];
-                                        printf("\n Remaining credit %i",credit);                
-                                }
-
-                                k_msleep(3000);
-                                state = BROWSING_MOVIES;
-                                break;
-                        }
-
-                        default:                                        //Error 
-                        break;
+                    credit = credit + 1;
+                    button_state[0] = 0;       
                 }
-        }   
+                else if(button_state[1] == 1)           // 2 EUR
+                {
+                    credit = credit + 2;
+                    button_state[1] = 0;                    
+                }
+                else if(button_state[2] == 1)           // 5 EUR
+                {
+                    credit = credit + 5;
+                    button_state[2] = 0;                   
+                }
+                else if(button_state[3] == 1)           // 10 EUR
+                {
+                    credit = credit + 10;            
+                    button_state[3] = 0;      
+                }
+                state = BROWSING_MOVIES;
+                break;
+            }
+            
+            case MOVIE_SELECTED:
+            {
+                printf("\033[2J\033[H");                // clear window code
+
+                printf("\n Ticket for movie %c, session %uH00 issued",movie_name[select_movie],movie_session[select_movie]);
+                if(movie_price[select_movie] > credit)
+                {
+                    printf("\n Not enought credit. Ticket not issued");                
+                }
+                else if(movie_price[select_movie] < credit)
+                {
+                    credit = credit-movie_price[select_movie];
+                    printf("\n Remaining credit %i",credit);                
+                }
+                k_msleep(3000);
+                state = BROWSING_MOVIES;
+                break;
+            }
+
+            case RETURN_CREDIT:
+            {
+                if(button_state[7] == 1)                // Return credit
+                {
+                    credit = 0;   
+                    printf("\033[2J\033[H");            // clear window code
+                    printf("\n %i EUR return",credit);
+                    k_msleep(3000);
+                    button_state[7] = 0;      
+                }
+                state = BROWSING_MOVIES;
+                break;
+            }
+
+            default:                                    //Error 
+            break;
+        }
+    }   
  
 }
